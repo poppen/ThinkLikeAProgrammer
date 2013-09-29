9b06f35661953a87ca970e7d37b03a0c99ed8fc1
poppen <poppen.jp@gmail.com>
2013-09-29 22:00:58 +0900

    change names of variables

--- chapter2/2-1.cpp
+++ chapter2/2-1.cpp
@@ -2,14 +2,14 @@
 
 int main(int argc, char const* argv[])
 {
-	int row = 8;
-	int line = 4;
+	int char_num = 8;
+	int row = 4;
 
-	for (int i = 0; i < line; i++) {
+	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < i; j++) {
 			std::cout << " ";
 		}
-		for (int j = 0; j < row - i * 2; j++) {
+		for (int j = 0; j < char_num - i * 2; j++) {
 			std::cout << "#";
 		}
 		std::cout << std::endl;
